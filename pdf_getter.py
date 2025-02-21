d60903a67b8b553146fdf867e0e7dd659032702b
Gwenn Durpoix-Espinasson <g.durpoix-espinasson@ecam.fr>
2025-02-21 10:02:49 +0100

    test pymupdf

--- pdf_getter.py
+++ pdf_getter.py
@@ -1,7 +1,20 @@
-from pypdf import PdfReader
-import pdfplumber
+from pypdf import PdfReader, PdfWriter
 
 PATH_TO_PDF = "./etl_sample.pdf"
+
+def normalize_pdf(input_pdf, output_pdf="normalized.pdf"):
+    reader = PdfReader(input_pdf)
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        writer.add_page(page)
+
+    with open(output_pdf, "wb") as f:
+        writer.write(f)
+
+    return output_pdf
+
+normalized_pdf = normalize_pdf(PATH_TO_PDF)
 
 reader = PdfReader(PATH_TO_PDF)
 txt = ""
@@ -11,15 +24,5 @@ for page in reader.pages:
     print(page.extract_text())
     txt+= page.extract_text()
 
-with open("./extracted_pypdf.txt", "w") as f:
-    f.write(txt)
-
-txt = ""
-with pdfplumber.open(PATH_TO_PDF) as pdf:
-    for page in pdf.pages:
-        print("PAGE NUMBER ",page.page_number)
-        print(page.extract_text())
-        txt+= page.extract_text()
-
-with open("./extracted_pdfplumber.txt", "w") as f:
+with open("./extracted_pypdf_norm.txt", "w") as f:
     f.write(txt)
