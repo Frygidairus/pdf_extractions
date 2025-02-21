8d1d023e1e817650f2f38674d9cabb72163196df
Gwenn Durpoix-Espinasson <g.durpoix-espinasson@ecam.fr>
2025-02-21 14:05:22 +0100

    make pdftextractor a standalone function

--- pdf_getter.py
+++
@@ -1,4 +0,0 @@
-from pdftextractor import PdfTextractor
-
-PATH_TO_PDF = "./etl_sample.pdf"
-
--- pdftextractor.py
+++ pdftextractor.py
@@ -1,35 +1,33 @@
 import pymupdf
 import re
 
-class PdfTextractor:
+def extract_text(path_to_pdf):
 
-    def __init__(self, path_to_pdf):
-        self.pdf = pymupdf.open(path_to_pdf)
+    pdf = pymupdf.open(path_to_pdf)
+    extracted_text = []
 
-    def extract_text(self):
+    for page in pdf:
 
-        extracted_text = []
-        pdf = self.pdf 
+        text = page.get_text("text")  # Extract text as plain text with line breaks
+        lines = text.split("\n")  # Split into lines for easier processing
+        cleaned_lines = []
 
-        for page in pdf:
-
-            text = page.get_text("text")  # Extract text as plain text with line breaks
-            lines = text.split("\n")  # Split into lines for easier processing
-            cleaned_lines = []
-
-            #Filter out page numbers and captions
-            for line in lines: #strips lines to remove unwanted spaces before checks
-                if not line.strip().isdigit() and not line.strip().startswith("Figure"):
-                    cleaned_lines.append(line)
-            
-            extracted_text.append(" ".join(cleaned_lines))  #Rejoin text from a page
-            
-        full_text = " ".join(extracted_text) #Rejoin text from all pages
-        clean_text = re.sub(r"\s+(\. ?\. ?\.|[.,!?:;])", r"\1", full_text) #Handle spaces before ponctuation
+        #Filter out page numbers and captions
+        for line in lines: #strips lines to remove unwanted spaces before checks
+            if not line.strip().isdigit() and not line.strip().startswith("Figure"):
+                cleaned_lines.append(line)
         
-        return clean_text  # Return full cleaned text
+        extracted_text.append(" ".join(cleaned_lines))  #Rejoin text from a page
+        
+    full_text = " ".join(extracted_text) #Rejoin text from all pages
+    clean_text = re.sub(r"\s+(\. ?\. ?\.|[.,!?:;])", r"\1", full_text) #Handle spaces before ponctuation
+    
+    return clean_text  # Return full cleaned text
 
-pdf = PdfTextractor("etl_sample.pdf")
+if __name__ == "__main__":
 
-text = pdf.extract_text()
-print(text)
+    text = extract_text("etl_sample.pdf")
+    print(text)
+
+    with open("./extraction_test.txt", "w") as file:
+        file.write(text)
